8e828762cd925848af5f513b8b20674ba707e912
averylam1996@gmail.com <averylamm@gmail.com>
2021-09-11 18:05:29 -0700

    monitor & send discord alert

--- app.js
+++ app.js
@@ -1,19 +1,106 @@
 const Web3 = require('web3')
+const axios = require('axios');
+const abiDecoder = require('abi-decoder');
+require('dotenv').config()
 
-const ADDRESSES = []
-const NETWORK = 'mainnet' //
-const INFURA_API_KEY = '9a6abeb243a24b7388082824b2838ea0'
-
-const provider = new Web3.providers.HttpProvider(`https://${NETWORK}.infura.io/v3/${INFURA_API_KEY}`)
+const provider = new Web3.providers.HttpProvider(`https://${process.env.NETWORK}.infura.io/v3/${process.env.INFURA_API_KEY}`)
 const web3 = new Web3(provider)
+let lastBlock
 
 async function checkLatestBlock() {
     let block = await web3.eth.getBlock('latest')
-    block.transactions.forEach(async (txn)=>{
-        let receipt = await web3.eth.getTransactionReceipt(txn)
-        console.log(receipt)
+    if (lastBlock === block.number) {
+        console.log("Old block, skipping", block.number)
+        return
+    }
+    lastBlock = block.number
+    console.log(`Reading ${block.number}`)
+
+    block.transactions.forEach(async (txn) => {
+
+        let tx = await web3.eth.getTransaction(txn)
+        // console.log(tx)
+        if (process.env.ADDRESSES.includes(tx.from.toLowerCase())){
+            let abi = await getABI(tx.to)
+            abiDecoder.addABI(JSON.parse(abi));
+            const decodedData = abiDecoder.decodeMethod(tx.input);
+            const logs = await web3.eth.getTransactionReceipt(txn)
+            // const decodedLogs = abiDecoder.decodeLogs(logs.logs);
+            // console.log(decodedLogs)
+            let txnData = {
+                tx,
+                inputData: decodedData
+            }
+            sendDiscordAlert(txnData)
+        }
     })
-    // console.log(block)
+
+}
+
+
+async function getABI(contractAddress) {
+    console.log(`Getting ABI of ${contractAddress}`)
+    let config = {
+        method: 'get',
+        url: `https://api.etherscan.io/api?module=contract&action=getabi&address=${contractAddress}&apikey=${process.env.ETHERSCAN_API_KEY}`,
+        headers: { }
+    };
+    let resp = await axios(config)
+    return resp.data.result
+}
+
+
+function sendDiscordAlert(data){
+    // console.log(data.inputData.params)
+    let d = {
+        "content": null,
+        "embeds": [
+            {
+                "title": `Transaction Found - ${data.tx.from}`,
+                "url": `https://etherscan.io/tx/${data.tx.hash}`,
+                "color": 65442,
+                "fields": [
+                    {
+                        "name": "Function called",
+                        "value": data.inputData.name
+                    },{
+                        "name": "Value",
+                        "value": web3.utils.fromWei(data.tx.value, 'ether')
+                    }
+                ]
+            }
+        ]
+    }
+
+    for (let param of data.inputData.params){
+        d.embeds[0].fields.push({
+            "name": param.name,
+            value: String(param.value)
+        })
+    }
+    let config = {
+        method: 'post',
+        url: process.env.DISCORD_WEBHOOK,
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        data : JSON.stringify(d)
+    };
+    // console.log(config)
+
+    axios(config)
+        .then((response) => {
+            console.log(JSON.stringify(response.data));
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+
 }
 
 checkLatestBlock()
+setInterval(()=>{
+    checkLatestBlock()
+}, parseInt(process.env.CHECK_INTERVAL) * 1000)
+
+
